1201ccdfea9d1ed422bd78870676ae84c72bc8df
Md Rahman <rahmanmd954@gmail.com>
2023-12-22 20:47:38 -0600

    fixed button names, and removeButton function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 //Authors Md Rahman & El-Shinawy 
 //current changes needed:   
 /* 
-
+  have to find a way to scrape UT registration to get the status of the class
 */
 let uniqueId;
 let currentStatus = "Unknown";
@@ -9,11 +9,11 @@ const ids = new Map();
 //number of notifications the user can have active at once
 const limit = 20;
 //function upon user click of add button
-document.getElementById("submitButton1").onclick = function () {
+document.getElementById("addButton").onclick = function () {
     uniqueId = document.getElementById("input").value;
     if (uniqueId.length != 6) {
         uniqueId = "";
-        console.log("Error Unique is was not entered correctly, or does not exist");
+        console.log("Error: Unique is was not entered correctly, or does not exist");
         document.getElementById("label").innerHTML = "Error Id not found";
         document.getElementById("label").classList.remove("success");
         document.getElementById("label").classList.add("error");
@@ -22,7 +22,7 @@ document.getElementById("submitButton1").onclick = function () {
         document.getElementById("label").classList.remove("success");
         document.getElementById("label").classList.add("error");
     } else {
-        //have to find a way to scrape UT registration to get the status of the class
+        
         console.log("Unique Id was found");
         document.getElementById("label").innerHTML = "Success!";
         document.getElementById("label").classList.remove("error");
@@ -32,11 +32,11 @@ document.getElementById("submitButton1").onclick = function () {
 }
  
 //function upon user click of remove button
-document.getElementById("submitButton2").onclick = function () {
+document.getElementById("removeButton").onclick = function () {
     uniqueId = document.getElementById("input").value;
     if (uniqueId.length != 6 || !ids.has(uniqueId)) {
         uniqueId = "";
-        console.log("Error Unique is was not entered correctly, or does not exist");
+        console.log("Error: Unique is was not entered correctly, or does not exist");
         document.getElementById("label").innerHTML = "Error Id not found";
         document.getElementById("label").classList.remove("success");
         document.getElementById("label").classList.add("error");
@@ -45,7 +45,7 @@ document.getElementById("submitButton2").onclick = function () {
         document.getElementById("label").innerHTML = "Success!";
         document.getElementById("label").classList.remove("error");
         document.getElementById("label").classList.add("success");
-        ids.delete("uniqueId");
+        ids.delete(uniqueId);
     }
 }
 
