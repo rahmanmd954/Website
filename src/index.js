ad73738d1dc6fe4faa6688f35d0eb98578336479
Ranya El-Shinawy <ranyagold22@gmail.com>
2023-12-28 12:42:44 -0600

    added function to scrape website and imported puppeteer (library needed to scrape)

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@
 let uniqueId;
 let currentStatus = "Unknown";
 const ids = new Map();
+const puppeteer = require('puppeteer');
 //number of notifications the user can have active at once
 const limit = 20;
 //function upon user click of add button
@@ -60,11 +61,15 @@ document.getElementById("currentList").onclick = function () {
     const listPage = window.open("list.html", "_blank", "width=600,height=400");
 }
 
+async function scrape(url){
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    // change what is in parentheses to Xpath we are scraping
+    const [el] = await page.$x('//*[@id="search_area"]/div[2]/div[2]/form/div[2]/label')
+    const txt = await el.getProperty('textContent');
+    const rawTxt = await txt.jsonValue();
+}
 
-
-
-
-
-
-
-
+//link to ut registration page
+scrape('https://utdirect.utexas.edu/apps/registrar/course_schedule/20242/');
