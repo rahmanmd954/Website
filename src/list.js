02b9829c4022400eb183fd9cb03c02420328afa6
Ranya El-Shinawy <ranyagold22@gmail.com>
2024-01-08 20:11:06 -0600

    added the require selenium and redirecting the user

--- src/list.js
+++ src/list.js
@@ -6,11 +6,15 @@ const idsList = new Map(storedIdsList);
 
 // Display the list in the new tab
 const classListContainer = document.getElementById('classList');
+//const { Builder, By, Key, until } = require('selenium-webdriver');
 
 idsList.forEach(([uniqueId, status]) => {
     const listItem = document.createElement('li');
     listItem.textContent = `ID: ${uniqueId}, Status: ${status}`;
     classListContainer.appendChild(listItem);
+
+    // // redirect the user to the UT log On page
+   window.location.href = 'https://enterprise.login.utexas.edu/idp/profile/SAML2/Redirect/SSO?execution=e3s1';
+
+
 });
-
-
