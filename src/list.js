35bbb1182ba03899b35f6bad98dd2428a82bd690
Ranya El-Shinawy <ranyagold22@gmail.com>
2024-01-11 20:48:20 -0600

    changed to import

--- src/list.js
+++ src/list.js
@@ -17,24 +17,17 @@ idsList.forEach(([uniqueId, status]) => {
 });
 
 // necessary libraries
-const { Builder, By, Key, until} = require("selenium-webdriver");
-require("chromedriver");
-
+import { Builder, By, Key, until} from ("selenium-webdriver");
+import chromedriver from 'chromedriver';
 async function test_case() {
     let driver = await new Builder().forBrowser("chrome").build();
+    await driver.get("https://utdirect.utexas.edu/apps/registrar/course_schedule/20242/");
 
-    try {
-        await driver.get("https://utdirect.utexas.edu/apps/registrar/course_schedule/20242/");
+    // Wait for the "unique_number" element to be present for a maximum of 40 seconds
+    await driver.wait(until.elementLocated(By.name("unique_number")), 40000);
 
-        // Wait for the "unique_number" element to be present for a maximum of 40 seconds
-        await driver.wait(until.elementLocated(By.name("unique_number")), 40000);
-
-        // Once the element is present, send keys
-        await driver.findElement(By.name("unique_number")).sendKeys("12345", Key.RETURN);
-    } finally {
-        // Close the browser
-        await driver.quit();
-    }
+    // Once the element is present, send keys
+    await driver.findElement(By.name("unique_number")).sendKeys("12345", Key.RETURN);
 }
 
 test_case();
